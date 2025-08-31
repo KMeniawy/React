c168f7d20dc277e6a7a9938314f184e4ed4aa418
KMeniawy <karim.elmeniawy.99@gmail.com>
2025-08-31 12:43:52 +0300

    first commit

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <>
+      <div className="card">
+      is Active : {isActive ? 'Yes' : 'No'}
+      <button onClick={() => setIsActive(!isActive)}>Toggle Active</button>
+      </div>
+    </>
+  )
+}
+
+export default App
